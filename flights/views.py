b31bcd5b7d1d17ae95cc5c77cdc01b3d86b74622
mva75 <mvafromsib@yahoo.com>
2023-08-14 21:52:12 +0500

    Add two files

--- flights/views.py
+++ flights/views.py
@@ -20,6 +20,6 @@ def flight(request, flight_id):
 def book(request, flight_id):
     if request.method == "POST":
         flight = Flight.objects.get(pk=flight_id)
-        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))  
+        passenger = passenger.objects.get(pk=int(request.POST["passenger"]))  
         passenger.flights.add(flight)  
         return HttpResponseRedirect(reverse("flight", args=(flight.id,)))   
